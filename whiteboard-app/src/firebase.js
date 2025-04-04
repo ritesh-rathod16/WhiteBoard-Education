05118b4ce0c91435441fc5b3bb722fd577dd7945
Ritesh Rathod <riteshrathod016@gmail.com>
2025-04-05 00:57:05 +0530

    Update firebase.js

--- whiteboard-app/src/firebase.js
+++ whiteboard-app/src/firebase.js
@@ -1,1 +1,21 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+import { getAuth, GoogleAuthProvider, GithubAuthProvider } from "firebase/auth";
 
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  databaseURL: "YOUR_DATABASE_URL",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
+const githubProvider = new GithubAuthProvider();
+
+export { database, auth, googleProvider, githubProvider };
